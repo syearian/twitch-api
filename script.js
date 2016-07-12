57c44ec97ad421c23338856d2e4476a6ab20d6c8
Steven Yearian <syearian@gmail.com>
2016-07-12 15:31:47 -0500

    Fix code for name, stream, & channel vars

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
-var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-var logo = "";
+var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "comster404", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"]
+var logo = "https://static-cdn.jtvnw.net/jtv-static/404_preview-50x50.png";
 var name = "";
 var stream = "";
 var channel = "";
 
 function getLogo(user) {
-  var getLogoUrl = 'https://api.twitch.tv/kraken/users/' + user + '?callback=?'
-  $.getJSON(getLogoUrl, function(json) {
-    console.log(json);
-    if (json.logo === null) {
-      logo = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
-      name = json.display_name;
-    } else {
-      logo = json.logo;
-      name = json.display_name;
-    }
-  });
 }
 
-function placeContent() {
-  if (stream === "Offline") {
-    $('streamerList').append('<a href="' + channel + '" class="list-group-item list-group-item-info"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
-  } else if (stream === "Account Closed") {
-    $('streamerList').append('<a href="' + channel + '" class="list-group-item list-group-item-danger"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
+function placeContent(user) {
+  if (user === "comster404" || user === "brunofin") {
+    logo = "https://static-cdn.jtvnw.net/jtv-static/404_preview-50x50.png";
   } else {
-    $('streamerList').prepend('<a href="' + channel + '" class="list-group-item list-group-item-success"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
+    var getUserUrl = 'https://api.twitch.tv/kraken/users/' + user + '?callback=?'
+    $.getJSON(getUserUrl, function(json) {
+      logo = json.logo;
+    });
+  }
+      console.log(logo);
+  if (stream === "Offline") {
+    $('#streamerList').append('<a href="' + channel + '" class="list-group-item list-group-item-info"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
+  } else if (stream === "Account Closed") {
+    $('#streamerList').append('<a href="' + channel + '" class="list-group-item list-group-item-danger"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
+  } else {
+    $('#streamerList').prepend('<a href="' + channel + '" class="list-group-item list-group-item-success"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
   }
 }
 
-function getStream(user) {
+function getContent(user) {
   var getStreamUrl = 'https://api.twitch.tv/kraken/streams/' + user + '?callback=?'
   $.getJSON(getStreamUrl, function(data) {
-    console.log(data);
     if (!(data.status === 422)) {
-      getLogo(user);
       if (data.stream === null) {
         stream = "Offline";
       } else {
         stream = data.stream.game;
       }
     } else {
-      getLogo(user);
       stream = "Account Closed"
     }
-    placeContent();
+    if (data.display_name) {
+      name = data.display_name;
+    } else {
+      name = user;
+    }
+    // if (data.stream.channel.logo) {
+    //   logo = data.stream.channel.logo;
+    // }
+    channel = "https://www.twitch.tv/" + user;
+    placeContent(user);
   });
 }
 
 $(document).ready(function() {
-  $.each(usernames, function(key, val) {
-    
-  });
+  for (key in usernames) {
+    getContent(usernames[key]);
+  };
 });
