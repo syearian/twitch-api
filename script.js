0f42341542bf3ebe0e8d0af067abd24743cb6cb5
Steven Yearian <syearian@gmail.com>
2016-07-12 16:09:32 -0500

    Nest getLogo() inside getContent()
    It appears to work now.

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "comster404", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"]
-var logo = "https://static-cdn.jtvnw.net/jtv-static/404_preview-50x50.png";
+var logo = "";
+var noLogo = "https://static-cdn.jtvnw.net/jtv-static/404_preview-50x50.png";
 var name = "";
 var stream = "";
 var channel = "";
 
-function getLogo(user) {
+function getLogo(user, name, stream, channel) {
+  var getUserUrl = 'https://api.twitch.tv/kraken/users/' + user + '?callback=?'
+  $.getJSON(getUserUrl, {})
+    .done(function(json) {
+      console.log(name)
+      console.log(stream)
+      if (json.logo === null) {
+        logo = noLogo;
+      } else {
+      logo = json.logo;
+      }
+      console.log(name)
+      console.log(stream)
+      placeContent(logo, name, stream, channel);
+    });
 }
 
-function placeContent(user) {
-  if (user === "comster404" || user === "brunofin") {
-    logo = "https://static-cdn.jtvnw.net/jtv-static/404_preview-50x50.png";
-  } else {
-    var getUserUrl = 'https://api.twitch.tv/kraken/users/' + user + '?callback=?'
-    $.getJSON(getUserUrl, function(json) {
-      logo = json.logo;
-    });
-  }
-      console.log(logo);
+function placeContent(logo, name, stream, channel) {
   if (stream === "Offline") {
     $('#streamerList').append('<a href="' + channel + '" class="list-group-item list-group-item-info"><div class="row"><div class="col-xs-2 col-sm-1 text-center"><img src="' + logo + '" alt="logo"></div><div class="col-xs-3 col-sm-2 text-center"><p class="list-group-item-text">' + name + '</p></div><div class="col-xs-7 col-sm-9 text-center"><p class="list-group-item-text">' + stream + '</p></div></div></a>');
   } else if (stream === "Account Closed") {
@@ -28,27 +34,27 @@ function placeContent(user) {
 
 function getContent(user) {
   var getStreamUrl = 'https://api.twitch.tv/kraken/streams/' + user + '?callback=?'
-  $.getJSON(getStreamUrl, function(data) {
-    if (!(data.status === 422)) {
-      if (data.stream === null) {
-        stream = "Offline";
+  channel = "https://www.twitch.tv/" + user;
+  $.getJSON(getStreamUrl, {})
+    .done(function(data) {
+      if (!(data.status === 422)) {
+        if (data.stream === null) {
+          stream = "Offline";
+        } else {
+          stream = data.stream.game;
+        }
       } else {
-        stream = data.stream.game;
+        stream = "Account Closed"
       }
-    } else {
-      stream = "Account Closed"
-    }
-    if (data.display_name) {
-      name = data.display_name;
-    } else {
-      name = user;
-    }
-    // if (data.stream.channel.logo) {
-    //   logo = data.stream.channel.logo;
-    // }
-    channel = "https://www.twitch.tv/" + user;
-    placeContent(user);
-  });
+      if (data.display_name) {
+        name = data.display_name;
+      } else {
+        name = user;
+      }
+      console.log(name)
+      console.log(stream)
+      getLogo(user, name, stream, channel);
+    });
 }
 
 $(document).ready(function() {
