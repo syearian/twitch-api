f09579be583c2d49186beb63088ecbe4918823a2
Steven Yearian <syearian@gmail.com>
2016-07-12 13:18:47 -0500

    Add initial version of functions

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "brunofin", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"]
+var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+var logo = "";
+var name = "";
+var stream = "";
+
+function getLogo(user) {
+  var getLogoUrl = 'https://api.twitch.tv/kraken/users/' + user + '?callback=?'
+  $.getJSON(getLogoUrl, function(data) {
+    console.log(data);
+    if (data.logo === null) {
+      logo = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+      name = data.display_name;
+    } else {
+      logo = data.logo;
+      name = data.display_name;
+    }
+  });
+}
+
+function getStream(user) {
+  var getStreamUrl = 'https://api.twitch.tv/kraken/streams/' + user + '?callback=?'
+  $.getJSON(getStreamUrl, function(data) {
+    console.log(data);
+    if (!(data.status === 422)) {
+      getLogo();
+      if (data.stream === null) {
+        stream = "Offline";
+      } else {
+        stream = data.stream.game;
+      }
+    } else {
+      getLogo();
+      stream = "Account Closed"
+    }
+  });
+}
+
+$(document).ready(function() {
+  $.each(usernames, function(key, val) {
+    
+  });
+});
